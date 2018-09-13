0a5612134ba309bfc3a731449a9136c6e0d42b2b
pathmann <thomas.pathmann@googlemail.com>
2018-09-13 13:19:46 +0200

    add fzf to zsh config

---
+++ .zsh/fzf.zsh
@@ -0,0 +1,8 @@
+if [ -f "/usr/share/zsh/site-contrib/fzf.zsh" ]; then
+    source /usr/share/zsh/site-contrib/fzf.zsh
+fi
+
+if [ -f "/usr/share/zsh/site-functions/_fzf" ]; then
+    source /usr/share/zsh/site-functions/_fzf
+fi
+
