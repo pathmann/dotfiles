5e22bf8bb81c3a04259526f3cefc235797b2e474
pathmann <thomas.pathmann@gmail.com>
2018-09-19 18:38:37 +0200

    add fzf support on arch

--- .zsh/fzf.zsh
+++ .zsh/fzf.zsh
@@ -1,8 +1,12 @@
 if [ -f "/usr/share/zsh/site-contrib/fzf.zsh" ]; then
     source /usr/share/zsh/site-contrib/fzf.zsh
+elif [ -f "/usr/share/fzf/key-bindings.zsh" ]; then
+    source /usr/share/fzf/key-bindings.zsh
 fi
 
 if [ -f "/usr/share/zsh/site-functions/_fzf" ]; then
     source /usr/share/zsh/site-functions/_fzf
+elif [ -f "/usr/share/fzf/completion.zsh" ]; then
+    source /usr/share/fzf/completion.zsh
 fi
 
