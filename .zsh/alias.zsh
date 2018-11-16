f28eab8c97d0932764a83da2d6775e81f1c811c5
pathmann <thomas.pathmann@gmail.com>
2018-11-16 14:32:47 +0100

    use customizepkg automatically in yay

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,3 +1,4 @@
 alias "ls=ls --color=auto -h -al"
 alias "gksudo=sudo gksudo gksudo"
 alias "gcalctool=LC_NUMERIC=de_DE.UTF-8 gnome-calculator"
+alias "yay=yay --save --makepkg $HOME/bin/customizepkg-makepkg.sh"
