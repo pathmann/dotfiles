2239754b87ab1bdf63e56348caefbbb9674d63c1
pathmann <thomas.pathmann@googlemail.com>
2019-11-30 16:54:36 +0100

    move git dir to another directory, use config as git alias

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -2,3 +2,5 @@ alias "ls=ls --color=auto -h -al"
 alias "gksudo=sudo gksudo gksudo"
 alias "gcalctool=LC_NUMERIC=de_DE.UTF-8 gnome-calculator"
 alias "yay=yay --save --makepkg $HOME/bin/customizepkg-makepkg.sh"
+alias "config=/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+#checkout with: git clone git@github.com:pathmann/dotfiles.git --separate-git-dir=~/.dotfiles ~
