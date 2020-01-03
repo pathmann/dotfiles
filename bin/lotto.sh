5b2e116f3928c0f90ab12357db9bc1bc8fa25e29
Thomas Pathmann <thomas.pathmann@gmail.com>
2020-01-03 12:23:53 +0100

    add option to not print numbers

--- bin/lotto.sh
+++ bin/lotto.sh
@@ -3,21 +3,35 @@
 if [ $# -le 1 ] ; then
   echo "Usage: $0 <numbercount> <out-of> [<count>]"
   echo "Example: $0 7 77 1"
+  exit 1
 fi
 
 COUNT=1
 NC=$1
 OUTOF=$2
+PRINTNUMBERS=true
 
 if [ $# -gt 2 ] ; then
   COUNT=$3
+fi
+
+if [ $# -gt 3 ] ; then
+  if [ "$4" == "-n" ] ; then
+    PRINTNUMBERS=false
+  else
+    echo "unknown option passed: $4"
+    exit 1
+  fi
 fi
 
 MAX=$COUNT
 
 while [ $COUNT -gt 0 ]
 do
-  echo "$(($MAX-$COUNT +1 % $MAX)). " $(seq -w $OUTOF | sort -R | head -$NC | sort -n | fmt)
+  if [ "$PRINTNUMBERS" = true ] ; then
+    printf "$(($MAX-$COUNT +1 % $MAX)). "
+  fi
+  echo $(seq -w $OUTOF | sort -R | head -$NC | sort -n | fmt)
   COUNT=$(($COUNT-1))
 done
 
