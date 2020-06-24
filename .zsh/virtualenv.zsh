c8f70401f65666c7dafab59359d62ed76f819993
Thomas Pathmann <thomas.pathmann@gmail.com>
2020-06-24 19:08:04 +0200

    add virtualenvwrapper (for Ubuntu wsl)

--- .zsh/virtualenv.zsh
+++ .zsh/virtualenv.zsh
@@ -2,5 +2,9 @@ if [ -f /usr/bin/virtualenvwrapper.sh ]; then
     export WORKON_HOME=$HOME/.virtualenvs
     export PROJECT_HOME=$PROJECT_DIR/Python
     source /usr/bin/virtualenvwrapper.sh
+elif [ -f /usr/share/virtualenvwrapper/virtualenvwrapper.sh ]; then
+    export WORKON_HOME=$HOME/.virtualenvs
+    export PROJECT_HOME=$PROJECT_DIR/Python
+    source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 fi
 
