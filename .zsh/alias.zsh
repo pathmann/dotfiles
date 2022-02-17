ee307a9a2c316f71e4962f606b53a320fc04455f
pathmann <thomas.pathmann@gmail.com>
2022-02-17 15:33:31 +0100

    use al as default for exa

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,4 +1,5 @@
 alias "ls=ls --color=auto -h -al"
+alias "exa=exa -al"
 alias "gksudo=sudo gksudo gksudo"
 alias "gcalctool=LC_NUMERIC=de_DE.UTF-8 gnome-calculator"
 alias "yay=yay --save --makepkg $HOME/bin/customizepkg-makepkg.sh"
