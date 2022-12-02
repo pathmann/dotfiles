399bd7c93d93f6865d1957fca0dbf647670f3e57
pathmann <thomas.pathmann@gmail.com>
2022-12-02 19:45:18 +0100

    add array for dirs to prepend to PATH

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -1,13 +1,22 @@
+prefpathdirs=(
+	~/.cargo/bin
+)
+
 pathdirs=(
   ~/bin
   /usr/sbin
   /media/PROJECTS/osxcross/target/bin
   ~/.local/bin
-  ~/.cargo/bin
 )
 
-for dir in $pathdirs; do
-    if [ -d $dir ]; then
-        path+=$dir
+for dname in $prefpathdirs; do
+	if [ -d $dname ]; then
+		path=($dname $path)
+	fi
+done
+
+for dname in $pathdirs; do
+    if [ -d $dname ]; then
+        path+=$dname
     fi
 done
