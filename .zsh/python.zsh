79c584016a9c928389944dbeed31e6834654fe88
pathmann <thomas.pathmann@gmail.com>
2022-12-12 22:33:25 +0100

    set pythonpath to current pythons version

--- .zsh/python.zsh
+++ .zsh/python.zsh
@@ -1,2 +1,6 @@
-export PYTHONPATH="/home/thomas/.local/lib/python3.3/site-packages"
+if [ -f /usr/bin/python ]; then
+  PYMINOR=$(python -c 'import sys; print(sys.version_info.minor)')  
+  export PYTHONPATH="/home/thomas/.local/lib/python3.$PYMINOR/site-packages"
+fi
+
 export PYTHONSTARTUP="/home/thomas/.pythonstartup"
