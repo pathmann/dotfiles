8e320d1a4d242028f328bbf084e342ca6799ea4c
pathmann <thomas.pathmann@gmail.com>
2023-05-30 10:17:46 +0200

    echo the unaliased command before executing

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -38,3 +38,42 @@ alias "naut=nautilus .&"
 if [ -f /usr/bin/zoxide ]; then
   alias "cd=z"
 fi
+
+# Reveal the real command executed, credits to https://dev.to/equiman/reveal-the-command-behind-an-alias-with-zsh-4d96
+local cmd_alias=""
+
+# Reveal Executed Alias
+alias_for() {
+  [[ $1 =~ '[[:punct:]]' ]] && return
+  local search=${1}
+  local found="$( alias $search )"
+  if [[ -n $found ]]; then
+    found=${found//\\//} # Replace backslash with slash
+    found=${found%\'} # Remove end single quote
+    found=${found#"$search="} # Remove alias name
+    found=${found#"'"} # Remove first single quote
+    echo "${found} ${2}" | xargs # Return found value (with parameters)
+  else
+    echo ""
+  fi
+}
+
+expand_command_line() {
+  first=$(echo "$1" | awk '{print $1;}')
+  rest=$(echo ${${1}/"${first}"/})
+
+  if [[ -n "${first//-//}" ]]; then # is not hypen
+    cmd_alias="$(alias_for "${first}" "${rest:1}")" # Check if there's an alias for the command
+    if [[ -n $cmd_alias ]] && [[ "${cmd_alias:0:1}" != "." ]]; then # If there was and not start with dot
+      echo "${T_GREEN}❯ ${T_YELLOW}${cmd_alias}${F_RESET}" # Print it
+    fi
+  fi
+}
+
+pre_alias_validation() {
+  [[ $# -eq 0 ]] && return                    # If there's no input, return. Else...
+  expand_command_line "$@"
+}
+
+autoload -U add-zsh-hook
+add-zsh-hook preexec pre_alias_validation 
