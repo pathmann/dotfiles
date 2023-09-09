a0c89381a7c350969d21c3ca771fe1de977b7f95
pathmann <thomas.pathmann@gmail.com>
2023-09-09 14:29:48 +0200

    move python startup file to config folder

--- .zsh/python.zsh
+++ .zsh/python.zsh
@@ -1,6 +1,6 @@
 if [ -f /usr/bin/python ]; then
   PYMINOR=$(python -c 'import sys; print(sys.version_info.minor)')  
-  export PYTHONPATH="/home/thomas/.local/lib/python3.$PYMINOR/site-packages"
+  export PYTHONPATH="$HOME/.local/lib/python3.$PYMINOR/site-packages"
 fi
 
-export PYTHONSTARTUP="/home/thomas/.pythonstartup"
+export PYTHONSTARTUP="$HOME/.config/python/startup.py"
