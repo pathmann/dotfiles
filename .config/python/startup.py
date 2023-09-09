a3fd5ee2fed1c7db2a8d8e9586e129492f042af1
pathmann <thomas.pathmann@gmail.com>
2023-09-09 14:36:23 +0200

    move python history to cache dir

--- .config/python/startup.py
+++ .config/python/startup.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+import os
+import atexit
+from pathlib import Path
+
+
+HIST_DIR = os.getenv("XDG_CACHE_HOME", (Path.home() / ".cache"))
+HIST_FILE = Path(HIST_DIR) / "python_history"
+HIST_LEN = 0
+
+
+def save_history(prev_len, f):
+    new_len = readline.get_current_history_length()
+    readline.set_history_length(1000)
+    readline.append_history_file(new_len - prev_len, f)
+
 try:
     import readline
 except ImportError:
@@ -7,3 +22,12 @@ except ImportError:
 else:
     import rlcompleter
     readline.parse_and_bind("tab: complete")
+    
+    if HIST_FILE.exists():
+        readline.read_history_file(str(HIST_FILE))
+        HIST_LEN = readline.get_current_history_length()
+    else:
+        HIST_FILE.parent.mkdir(750, parents=True, exist_ok=True)
+        HIST_FILE.open("w").close()
+        
+    atexit.register(save_history, HIST_LEN, HIST_FILE)
