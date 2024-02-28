d7fe3c43b4d914499c558292473c55ab3705c77a
pathmann <thomas.pathmann@gmail.com>
2024-02-28 08:58:51 +0100

    reload the DISPLAY variable

--- .config/zsh/reload-environment.zsh
+++ .config/zsh/reload-environment.zsh
@@ -1,9 +1,10 @@
 if [ -n "$TMUX" ]; then                                                                               
   function refresh {                                                                                
-    export $(tmux show-environment | grep "^WAYLAND_DISPLAY")                                       
+    export $(tmux show-environment | grep "^WAYLAND_DISPLAY")
     export $(tmux show-environment | grep "^XDG_CURRENT_DESKTOP")
-	export $(tmux show-environment | grep "^HYPRLAND_INSTANCE_SIGNATURE")                                             
-    export $(tmux show-environment | grep "^SSH_AUTH_SOCK")                                       
+    export $(tmux show-environment | grep "^HYPRLAND_INSTANCE_SIGNATURE")
+    export $(tmux show-environment | grep "^SSH_AUTH_SOCK")
+    export $(tmux show-environment | grep "^DISPLAY")
   }                                                                                                 
 else                                                                                                  
   function refresh { }                                                                              
