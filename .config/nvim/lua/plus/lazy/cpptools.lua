dc1f2ef34c5d4d4c178d177930cd1e3848d287e2
Thomas Pathmann <thomas.pathmann@gmail.com>
2024-10-16 12:15:26 +0200

    add cpptools

---
+++ .config/nvim/lua/plus/lazy/cpptools.lua
@@ -0,0 +1,56 @@
+local function createClassDeclaration()
+  local ts_utils = require('nvim-treesitter.ts_utils')
+  local cpptools = require("nt-cpp-tools.internal")
+
+  local cursor_node = ts_utils.get_node_at_cursor()
+
+  while cursor_node do
+    if cursor_node:type() == 'class_specifier' then
+      ts_utils.goto_node(cursor_node)
+
+      local start_row, _, end_row, _ = cursor_node:range()
+      cpptools.imp_func(start_row +1, end_row +1)
+      vim.cmd("normal! G")
+      return
+    end
+
+    cursor_node = cursor_node:parent()
+  end
+
+  print("No class definition found under the cursor.")
+end
+
+return {
+  "Badhi/nvim-treesitter-cpp-tools",
+
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter"
+  },
+
+  config = function()
+    local plug = require("nt-cpp-tools")
+    plug.setup({
+      preview = {
+        quit = "q", -- optional keymapping for quit preview
+        accept = "<tab>", -- optional keymapping for accept preview
+      },
+      header_extension = "h", -- optional
+      source_extension = "cpp", -- optional
+      custom_define_class_function_commands = { -- optional
+        TSCppImplWrite = {
+          output_handle = require("nt-cpp-tools.output_handlers").get_add_to_cpp(),
+        },
+      },
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "cpp",
+      callback = function()
+        vim.keymap.set("n", "<leader>md", function()
+          createClassDeclaration()
+        end, { noremap = true, silent = true })
+      end,
+    })
+
+  end,
+}
