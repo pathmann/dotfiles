cf3299f7929af3d15ea92cc363a5a88e5524ba38
Thomas Pathmann <thomas.pathmann@gmail.com>
2024-10-17 09:25:00 +0200

    add yanky

---
+++ .config/nvim/lua/plus/lazy/yanky.lua
@@ -0,0 +1,24 @@
+return {
+  "gbprod/yanky.nvim",
+
+  config = function()
+    local opts = {
+
+    }
+
+    local y = require("yanky")
+    y.setup(opts)
+
+    vim.keymap.set({"n","x"}, "p", "<Plug>(YankyPutAfter)")
+    vim.keymap.set({"n","x"}, "P", "<Plug>(YankyPutBefore)")
+    vim.keymap.set({"n","x"}, "gp", "<Plug>(YankyGPutAfter)")
+    vim.keymap.set({"n","x"}, "gP", "<Plug>(YankyGPutBefore)")
+
+    vim.keymap.set("n", "<c-p>", "<Plug>(YankyPreviousEntry)")
+    vim.keymap.set("n", "<c-n>", "<Plug>(YankyNextEntry)")
+
+    vim.keymap.set("n", "<leader>pp", function()
+      require("telescope").extensions.yank_history.yank_history()
+    end)
+  end
+}
