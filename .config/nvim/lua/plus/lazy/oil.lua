62477e1a1d82b3f392f4ac783ec2bf7d8a3c06fd
pathmann <thomas.pathmann@gmail.com>
2024-10-20 11:25:25 +0200

    add oil.nvim

---
+++ .config/nvim/lua/plus/lazy/oil.lua
@@ -0,0 +1,25 @@
+return {
+  "stevearc/oil.nvim",
+
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+
+  config = function()
+    local oil = require("oil")
+    oil.setup({
+      default_file_explorer = false,
+      columns = {
+        "icons",
+      },
+      keymaps = {
+        ["<C-h>"] = false,
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    })
+
+    vim.keymap.set("n", "<leader>-", "<cmd>Oil<CR>", { desc = "Parent dir in oil" })
+  end,
+}
