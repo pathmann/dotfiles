a64cd421c2c0a59690403d0d730c2aecef94d9c5
Thomas Pathmann <thomas.pathmann@gmail.com>
2024-10-28 08:59:00 +0100

    disable indent folding in iec

--- .config/nvim/lua/plus/lazy/iec.lua
+++ .config/nvim/lua/plus/lazy/iec.lua
@@ -1,3 +1,12 @@
 return {
   "jubnzv/IEC.vim",
+
+  config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "iec",
+      callback = function()
+        vim.opt_local.foldmethod = "manual"
+      end,
+    })
+  end,
 }
