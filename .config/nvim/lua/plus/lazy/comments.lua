fded1e2cfa3ddbedbf8a080a7b254a40175a0841
Thomas Pathmann <thomas.pathmann@gmail.com>
2024-11-04 14:43:01 +0100

    add comment.nvim

---
+++ .config/nvim/lua/plus/lazy/comments.lua
@@ -0,0 +1,15 @@
+return {
+  "numToStr/Comment.nvim",
+
+  opts = {
+    toggler = {
+      line = "<leader>//",
+      block = "<leader>/*",
+    },
+    opleader = nil,
+    extra = nil,
+    mappings = {
+      extra = false,
+    }
+  }
+}
