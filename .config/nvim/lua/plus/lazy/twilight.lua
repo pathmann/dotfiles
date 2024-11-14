3fb1679572bbbcf861f7ee35d7fd427798f2088b
Thomas Pathmann <thomas.pathmann@gmail.com>
2024-11-14 11:31:42 +0100

    use different expand options when in cpp filetype

--- .config/nvim/lua/plus/lazy/twilight.lua
+++ .config/nvim/lua/plus/lazy/twilight.lua
@@ -2,24 +2,60 @@ return {
   "folke/twilight.nvim",
 
   config = function()
+    local expand = {
+      "function",
+      "method",
+      "table",
+      "if_statement",
+    }
+
+    local cppexpand = {
+      "function",
+      "method",
+      "function_declaration",
+      "function_definition",
+      "method_declaration",
+    }
+
     local opts = {
-      expand = {
-        "function",
-        "method",
-        "table",
-        "if_statement",
-        "function_definition",
-      },
+      expand = expand,
       exclude = {
         "text",
         "jinja",
         "text.jinja",
+        "markdown",
+      }
+    }
+
+    local cppopts = {
+      expand = cppexpand,
+      exclude = {
+        "text",
+        "jinja",
+        "text.jinja",
+        "markdown",
       }
     }
 
     local tw = require("twilight")
     tw.setup(opts)
 
-    tw.enable()
+    vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+      callback = function()
+        local ft = vim.bo.filetype
+        local uopts = opts
+        if ft == "cpp" then
+          uopts = cppopts
+        end
+
+        tw.setup(uopts)
+
+        if vim.tbl_contains(uopts.exclude, ft) then
+          tw.disable()
+        else
+          tw.enable()
+        end
+      end
+    })
   end
 }
