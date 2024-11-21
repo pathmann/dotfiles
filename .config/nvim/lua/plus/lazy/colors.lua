78e9beabadd37f73417294a9b76ef72833125bea
Thomas Pathmann <thomas.pathmann@gmail.com>
2024-11-21 09:41:24 +0100

    move colorscheme options

--- .config/nvim/lua/plus/lazy/colors.lua
+++ .config/nvim/lua/plus/lazy/colors.lua
@@ -1,5 +1,13 @@
 
 return {
   "folke/tokyonight.nvim",
+
+  config = function()
+    local color = "tokyonight"
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  end,
 }
 
