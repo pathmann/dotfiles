2acc60f96e51933670edff20e48dbf93feb8ceea
pathmann <thomas.pathmann@gmail.com>
2024-12-01 20:02:30 +0100

    use hop.nvim

---
+++ .config/nvim/lua/plus/lazy/hop.lua
@@ -0,0 +1,18 @@
+return {
+  "smoka7/hop.nvim",
+
+  config = function()
+    local hop = require('hop')
+    hop.setup({
+      keys = "asdertovxqpdygfblzhckiurn",
+    })
+
+    local directions = require('hop.hint').HintDirection
+    vim.keymap.set('', 'f', function()
+      hop.hint_char1({ direction = directions.AFTER_CURSOR})
+    end, {remap=true})
+    vim.keymap.set('', 'F', function()
+      hop.hint_char1({ direction = directions.BEFORE_CURSOR})
+    end, {remap=true})
+  end,
+}
