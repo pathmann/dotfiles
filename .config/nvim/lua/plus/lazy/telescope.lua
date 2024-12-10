78fbfb83ba12ad155168565238cc86ddbd8cc4d5
pathmann <thomas.pathmann@gmail.com>
2024-12-10 15:34:45 +0100

    use telescope-fzf-native

--- .config/nvim/lua/plus/lazy/telescope.lua
+++ .config/nvim/lua/plus/lazy/telescope.lua
@@ -4,10 +4,12 @@ return {
   tag = '0.1.8',
 
   dependencies =  {
-    'nvim-lua/plenary.nvim'
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
 
   config = function()
+    require("telescope").setup({})
     local builtin = require('telescope.builtin')
 
     vim.keymap.set('n', '<leader>ff', function()
