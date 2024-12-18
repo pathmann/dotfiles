d06a37a779508ca128bc7172e3b79bcf0e319079
pathmann <thomas.pathmann@gmail.com>
2024-12-18 14:23:13 +0100

    add mini.ai textobject for function definition

--- .config/nvim/lua/plus/lazy/mini-ai.lua
+++ .config/nvim/lua/plus/lazy/mini-ai.lua
@@ -2,4 +2,18 @@ return {
   "echasnovski/mini.nvim",
 
   version = "*",
+
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+
+  config = function()
+    local spec_treesitter = require('mini.ai').gen_spec.treesitter
+    local mini = require("mini.ai")
+    mini.setup({
+      custom_textobjects = {
+        F = spec_treesitter({ a = '@function.outer', i = '@function.inner' }),
+      }
+    })
+  end,
 }
