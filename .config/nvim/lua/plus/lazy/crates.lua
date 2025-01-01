4d5efc5058fb832e491e2e5ee4013c88b9bea0ec
pathmann <thomas.pathmann@gmail.com>
2025-01-01 20:45:17 +0100

    add crates.nvim

---
+++ .config/nvim/lua/plus/lazy/crates.lua
@@ -0,0 +1,32 @@
+return {
+  "saecki/crates.nvim",
+
+  event = { "BufRead Cargo.toml" },
+
+  config = function()
+    require("crates").setup({
+      completion = {
+        cmp = {
+          enabled = true,
+        },
+        crates = {
+          enabled = true,
+          max_results = 8,
+          min_chars = 3,
+        },
+        null_ls = {
+          enabled = true,
+          name = "crates.nvim",
+        },
+      }
+    })
+
+    vim.api.nvim_create_autocmd("BufRead", {
+      group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
+      pattern = "Cargo.toml",
+      callback = function()
+        require("cmp").setup.buffer({ sources = { { name = "crates" } } })
+      end,
+    })
+  end,
+}
