94860acd2a7ec6ba5aa2ac7c3711c8a140dad375
pathmann <thomas.pathmann@gmail.com>
2025-01-07 08:01:16 +0100

    add keymap for selecting session; fix keymap descriptions

--- .config/nvim/lua/plus/lazy/persistence.lua
+++ .config/nvim/lua/plus/lazy/persistence.lua
@@ -8,7 +8,10 @@ return {
 
     vim.keymap.set("n", "<leader>ql", function()
       per.load()
-    end, { desc = "Select/Load session" })
+    end, { desc = "Load session" })
+    vim.keymap.set("n", "<leader>qL", function()
+      per.select()
+    end, { desc = "Select session" })
     vim.keymap.set("n", "<leader>qs", function()
       per.save()
     end, { desc = "Store session" })
