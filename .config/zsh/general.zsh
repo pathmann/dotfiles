f2a0975e7d212d9b4032a5e65bfc8e09eb663a81
Thomas Pathmann <thomas.pathmann@gmail.com>
2025-01-23 08:58:30 +0100

    set EDITOR to nvim,vim,vi or nano (micro)

--- .config/zsh/general.zsh
+++ .config/zsh/general.zsh
@@ -1,4 +1,12 @@
 export PROJECT_DIR=/media/PROJECTS/Active/
 setopt autocd
 
-export EDITOR=nano
+if [ -f /usr/bin/nvim ]; then
+  export EDITOR=nvim
+elif [ -f /usr/bin/vim ]; then
+  export EDITOR=vim
+elif [ -f /usr/bin/vi ]; then
+  export EDITOR=vi
+else
+  export EDITOR=nano
+fi
