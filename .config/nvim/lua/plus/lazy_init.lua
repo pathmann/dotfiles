5c48c1bf103bf49c85f256cb6bb74336ce8db47b
Thomas Pathmann <thomas.pathmann@gmail.com>
2025-02-15 09:27:00 +0100

    add possibility to apply local (from <pluginname>_options.lua in cwd) plugin options

--- .config/nvim/lua/plus/lazy_init.lua
+++ .config/nvim/lua/plus/lazy_init.lua
@@ -16,6 +16,28 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local lazyloader = require("lazy.core.loader")
+local config_original = lazyloader.config
+
+---@param plugin LazyPlugin
+lazyloader.config = function(plugin)
+  local path = vim.fn.getcwd() .. "/." .. plugin.name .. "_options.lua"
+  if vim.fn.filereadable(path) == 1 then
+    local locopts = loadfile(path)
+    if type(locopts) == "function" then
+      local cwdopts = locopts()
+      if type(cwdopts) ~= "table" then
+        vim.notify("local plugin opts for " .. plugin.name .. " did not return a table")
+      else
+        --I hope this not interfere with lazy's plugin cache
+        plugin.opts = vim.tbl_deep_extend("force", plugin.opts or {}, cwdopts)
+      end
+    end
+  end
+
+  config_original(plugin)
+end
+
 require("lazy").setup({
     spec = "plus.lazy",
     change_detection = { notify = false },
@@ -24,3 +46,4 @@ require("lazy").setup({
       fallback = false,
     }
 })
+
