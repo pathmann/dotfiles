ace244dfc2fa6d94d8b925d3aceea11d06cb0c6b
Thomas Pathmann <thomas.pathmann@gmail.com>
2025-02-24 09:40:00 +0100

    use clang-format to format function implementation

--- .config/nvim/lua/plus/lazy/cpptools.lua
+++ .config/nvim/lua/plus/lazy/cpptools.lua
@@ -20,6 +20,52 @@ local function createClassDeclaration()
   print("No class definition found under the cursor.")
 end
 
+local function clang_format(input_str)
+  return vim.fn.system("echo ' " .. input_str .. "' | clang-format")
+end
+
+---unfortunately clangd seems to not notice PointerAlignment, ReferenceAlignment or DerivcePointerAlignment
+local function clangd_format(input_str)
+  local buf = vim.api.nvim_create_buf(false, true) -- (listed=false, scratch=true)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(input_str, "\n"))
+  vim.api.nvim_set_option_value("filetype", "cpp", { buf = buf })
+  vim.api.nvim_buf_set_name(buf, "/tmp/test-" .. buf .. ".cpp")
+
+  local params = vim.lsp.util.make_formatting_params({always_format = true})
+  params.textDocument.uri = vim.uri_from_bufnr(buf)
+
+  local client = vim.lsp.get_clients({ name = "clangd" })[1]
+  if not client or not client.supports_method("textDocument/formatting") then
+    print("clangd not found or not supported")
+    vim.api.nvim_buf_delete(buf, { force = true })
+    return input_str
+  end
+
+  if not vim.lsp.buf_is_attached(buf, client.id) then
+    vim.lsp.buf_attach_client(buf, client.id)
+  end
+
+  -- Send synchronous formatting request
+  local result = vim.lsp.buf_request_sync(buf, "textDocument/formatting", params, 1000)
+
+  if result and result[client.id] and result[client.id].result then
+    local edits = result[client.id].result
+
+    vim.lsp.util.apply_text_edits(edits, buf, "utf-8")
+
+    local formatted_lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+
+    vim.lsp.buf_detach_client(buf, client.id)
+    vim.api.nvim_buf_delete(buf, { force = true })
+
+    return table.concat(formatted_lines, "\n")
+  else
+    vim.lsp.buf_detach_client(buf, client.id)
+    vim.api.nvim_buf_delete(buf, { force = true })
+    return input_str
+  end
+end
+
 return {
   "Badhi/nvim-treesitter-cpp-tools",
 
@@ -39,7 +85,7 @@ return {
       custom_define_class_function_commands = { -- optional
         TSCppDefineClassFunc = {
           output_handle = function(output, ctx)
-            output = output:gsub("\n{\n}\n", " {\n\n}\n\n")
+            output = clang_format(output)
             require("nt-cpp-tools.output_handlers").get_preview_and_apply()(output, ctx)
           end
         },
