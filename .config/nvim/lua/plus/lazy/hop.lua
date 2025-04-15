35505ca9aa43c66c064a842fd49ce0cc89a561b9
pathmann <thomas.pathmann@gmail.com>
2025-04-15 11:31:56 +0200

    use hop keymaps only in normal mode

--- .config/nvim/lua/plus/lazy/hop.lua
+++ .config/nvim/lua/plus/lazy/hop.lua
@@ -8,10 +8,10 @@ return {
     })
 
     local directions = require('hop.hint').HintDirection
-    vim.keymap.set('', 'f', function()
+    vim.keymap.set('n', 'f', function()
       hop.hint_char1({ direction = directions.AFTER_CURSOR})
     end, {remap=true})
-    vim.keymap.set('', 'F', function()
+    vim.keymap.set('n', 'F', function()
       hop.hint_char1({ direction = directions.BEFORE_CURSOR})
     end, {remap=true})
   end,
