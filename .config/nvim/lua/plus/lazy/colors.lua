38d13cf0f4c973c0e5457106c0a305a6febf28b9
pathmann <thomas.pathmann@gmail.com>
2025-05-16 09:48:08 +0200

    use kitty instead of ghostty as default terminal emulator

--- .config/nvim/lua/plus/lazy/colors.lua
+++ .config/nvim/lua/plus/lazy/colors.lua
@@ -2,21 +2,38 @@
 return {
   "folke/tokyonight.nvim",
 
-  config = function()
+  opts = {
+    transparent = true,
+  },
+
+  config = function(_, opts)
+    require("tokyonight").setup(opts)
+
     local color = "tokyonight"
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
     vim.api.nvim_create_user_command("Day", function()
       vim.cmd.colorscheme("tokyonight-day")
+
+      vim.uv.spawn("kitten", {
+        args = { "@", "--to", "unix:/tmp/mainkitty", "set-colors", "-c", vim.env.HOME .. "/.config/kitty/light-theme.conf" }
+      })
+
+      vim.api.nvim_create_autocmd({ 'VimLeave' }, {
+        group = vim.api.nvim_create_augroup("KittyThemeReset", { clear = true }),
+        callback = function()
+          vim.uv.spawn("kitten", {
+            args = { "@", "--to", "unix:/tmp/mainkitty", "set-colors", "-c", vim.env.HOME .. "/.config/kitty/dark-theme.conf" }
+          })
+        end,
+      })
     end, {})
     vim.api.nvim_create_user_command("Night", function()
-      vim.cmd.colorscheme("tokyonight-day")
+      vim.cmd.colorscheme("tokyonight")
 
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      vim.uv.spawn("kitten", {
+        args = { "@", "--to", "unix:/tmp/mainkitty", "set-colors", "-c", vim.env.HOME .. "/.config/kitty/dark-theme.conf" }
+      })
     end, {})
   end,
 }
