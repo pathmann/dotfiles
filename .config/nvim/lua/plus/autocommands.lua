feb3036e4972cf15fbc5e6207888997985855d53
pathmann <thomas.pathmann@gmail.com>
2025-07-04 15:25:54 +0200

    add auto commands: close some buffers with q; create intermediate dirs if not exist when saving

---
+++ .config/nvim/lua/plus/autocommands.lua
@@ -0,0 +1,29 @@
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+-- Copyright: https://github.com/Isrothy/dotfile/blob/master/.config/nvim/lua/isrothy/autocmds.lua
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
+  callback = function(event)
+    if event.match:match("^%w%w+:[\\/][\\/]") then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
+-- close some filetypes with <q>
+-- Copyright: https://github.com/Isrothy/dotfile/blob/master/.config/nvim/lua/isrothy/autocmds.lua
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
+  pattern = {
+    "help",
+    "lspinfo",
+    "startuptime",
+    "tsplayground",
+    "checkhealth",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
