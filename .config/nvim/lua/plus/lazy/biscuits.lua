4a11046672030aaff213ce2ac46f18a69a51736c
pathmann <thomas.pathmann@gmail.com>
2025-07-30 07:54:02 +0200

    use nvim-biscuits

---
+++ .config/nvim/lua/plus/lazy/biscuits.lua
@@ -0,0 +1,14 @@
+return {
+  "code-biscuits/nvim-biscuits",
+
+  opts = {
+    cursor_line_only = true,
+    min_distance = 0,
+
+    language_config = {
+      python = {
+        disabled = true,
+      }
+    }
+  },
+}
