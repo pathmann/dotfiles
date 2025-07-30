4bce106b46d19555b58d3c39bc3f5fa81734464c
pathmann <thomas.pathmann@gmail.com>
2025-07-30 07:43:40 +0200

    turn off relative line numbers when in insert mode

--- .config/nvim/lua/plus/autocommands.lua
+++ .config/nvim/lua/plus/autocommands.lua
@@ -28,3 +28,15 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
   end,
 })
+
+vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+  callback = function()
+    vim.opt.relativenumber = false
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+  callback = function()
+    vim.opt.relativenumber = true
+  end,
+})
