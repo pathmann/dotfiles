7c46a44102914309941f25c835e5f663632993d8
pathmann <thomas.pathmann@gmail.com>
2025-08-04 08:49:47 +0200

    load project related snippets from cwd/.nvim-snippets

--- .config/nvim/lua/plus/lazy/snippets.lua
+++ .config/nvim/lua/plus/lazy/snippets.lua
@@ -16,6 +16,11 @@ return {
     local snippets_path = vim.fn.stdpath("config") .. "/lua/plus/snippets"
     require("luasnip.loaders.from_lua").load({ paths = { snippets_path } })
 
+    local projpath = vim.fn.getcwd() .. "/.nvim-snippets"
+    if vim.fn.isdirectory(projpath) ~= 0 then
+      require("luasnip.loaders.from_lua").load({ paths = { projpath } })
+    end
+
     vim.keymap.set({"i"}, "<C-K>", function()
       snip.expand()
     end, {silent = true})
