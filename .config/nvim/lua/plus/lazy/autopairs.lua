37623e15385e38da4d34f3309cd2f811dd4aab78
pathmann <thomas.pathmann@gmail.com>
2025-08-04 08:32:09 +0200

    add some jinja autopairs

--- .config/nvim/lua/plus/lazy/autopairs.lua
+++ .config/nvim/lua/plus/lazy/autopairs.lua
@@ -1,5 +1,21 @@
 return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
-  config = true
+
+  opts = {
+
+  },
+
+  config = function(_, opts) 
+    local plug = require("nvim-autopairs")
+    local Rule = require("nvim-autopairs.rule")
+
+    plug.setup(opts)
+
+    plug.get_rules("{")[1].not_filetypes = { "jinja" }
+    plug.add_rule(Rule("{%- if -%}", "{%- endif -%}", "jinja"))
+    plug.add_rule(Rule("{% if %}", "{% endif %}", "jinja"))
+    plug.add_rule(Rule("{%- for -%}", "{%- endfor -%}", "jinja"))
+    plug.add_rule(Rule("{% for %}", "{% endfor %}", "jinja"))
+  end
 }
