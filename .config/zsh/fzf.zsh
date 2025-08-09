87390a5f5e0a2d50b8c746b65df80376c7c84d0e
pathmann <thomas.pathmann@gmail.com>
2025-08-09 18:37:33 +0200

    simplify fzf sourcing; remamp fzf-file-widget to Ctrl+f

--- .config/zsh/fzf.zsh
+++ .config/zsh/fzf.zsh
@@ -1,12 +1,6 @@
-if [ -f "/usr/share/zsh/site-contrib/fzf.zsh" ]; then
-    source /usr/share/zsh/site-contrib/fzf.zsh
-elif [ -f "/usr/share/fzf/key-bindings.zsh" ]; then
-    source /usr/share/fzf/key-bindings.zsh
-fi
+if [ -f /usr/bin/fzf ]; then
+  source <(fzf --zsh)
 
-if [ -f "/usr/share/zsh/site-functions/_fzf" ]; then
-    source /usr/share/zsh/site-functions/_fzf
-elif [ -f "/usr/share/fzf/completion.zsh" ]; then
-    source /usr/share/fzf/completion.zsh
+  bindkey -r '^T'
+  bindkey '^f' fzf-file-widget
 fi
-
