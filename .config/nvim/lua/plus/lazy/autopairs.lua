49660ce3d73205f12d5ab731b43700557903a11f
pathmann <thomas.pathmann@gmail.com>
2025-08-12 09:07:47 +0200

    use jinja autopairs in text.jinja filetype too

--- .config/nvim/lua/plus/lazy/autopairs.lua
+++ .config/nvim/lua/plus/lazy/autopairs.lua
@@ -12,22 +12,23 @@ return {
 
     plug.setup(opts)
 
-    plug.get_rules("{")[1].not_filetypes = { "jinja" }
+    local jinjafts = {"jinja", "text.jinja"}
+    plug.get_rules("{")[1].not_filetypes = jinjafts
     -- with_pair: avoid appending more end tags when typing between start and end, only when '}' is typed,
     -- but I don't know how to get only_cr to work
-    plug.add_rule(Rule("{%%%- if.- %-%%}", "{%- endif -%}", "jinja"):use_regex(true)
+    plug.add_rule(Rule("{%%%- if.- %-%%}", "{%- endif -%}", jinjafts):use_regex(true)
     :with_pair(function(fopts)
       return fopts.char == "}"
     end))
-    plug.add_rule(Rule("{%% if.- %%}", "{% endif %}", "jinja"):use_regex(true)
+    plug.add_rule(Rule("{%% if.- %%}", "{% endif %}", jinjafts):use_regex(true)
     :with_pair(function(fopts)
       return fopts.char == "}"
     end))
-    plug.add_rule(Rule("{%%%- for.- %-%%}", "{%- endfor -%}", "jinja"):use_regex(true)
+    plug.add_rule(Rule("{%%%- for.- %-%%}", "{%- endfor -%}", jinjafts):use_regex(true)
     :with_pair(function(fopts)
       return fopts.char == "}"
     end))
-    plug.add_rule(Rule("{%% for.- %%}", "{% endfor %}", "jinja"):use_regex(true)
+    plug.add_rule(Rule("{%% for.- %%}", "{% endfor %}", jinjafts):use_regex(true)
     :with_pair(function(fopts)
       return fopts.char == "}"
     end))
