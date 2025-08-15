08d112dfe7ef1053641a380146911666368319b2
pathmann <thomas.pathmann@gmail.com>
2025-08-15 08:48:45 +0200

    add current selection of dap-project-configuration to lualine

--- .config/nvim/lua/plus/lazy/lualine.lua
+++ .config/nvim/lua/plus/lazy/lualine.lua
@@ -6,6 +6,18 @@ return {
   },
 
   opts = {
-
+    sections = {
+      lualine_c = {
+        "filename",
+        function()
+          local ok, sel = pcall(require, "dap-project-configuration")
+          if ok and sel and sel.current_selection then
+            return sel.current_selection
+          else
+            return ""
+          end
+        end
+      }
+    }
   },
 }
