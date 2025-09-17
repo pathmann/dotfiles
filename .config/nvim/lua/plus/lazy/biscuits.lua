afa82bfbf0dd7ef2a8d68db06321c2c8f4d9d18e
pathmann <thomas.pathmann@gmail.com>
2025-09-17 13:10:19 +0200

    add tree-sitter as dependency of nvim-biscuits; show it on start; disable in vimdoc and query filetype

--- .config/nvim/lua/plus/lazy/biscuits.lua
+++ .config/nvim/lua/plus/lazy/biscuits.lua
@@ -1,7 +1,12 @@
 return {
   "code-biscuits/nvim-biscuits",
 
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+
   opts = {
+    show_on_start = true,
     cursor_line_only = true,
     min_distance = 0,
     max_file_size = '100kb',
@@ -9,7 +14,13 @@ return {
     language_config = {
       python = {
         disabled = true,
-      }
+      },
+      vimdoc = {
+        disabled = true,
+      },
+      query = {
+        disabled = true,
+      },
     }
   },
 }
