87556b4e5ba9337adce9d3a9dc159b22a3c4e668
pathmann <thomas.pathmann@gmail.com>
2025-09-17 13:08:54 +0200

    close git blame with q

--- .config/nvim/lua/plus/autocommands.lua
+++ .config/nvim/lua/plus/autocommands.lua
@@ -23,6 +23,7 @@ vim.api.nvim_create_autocmd("FileType", {
     "checkhealth",
     "git",
     "qf",
+    "fugitiveblame",
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
