814a1962cbb440a0ff0b036f7259155aa6593b77
pathmann <thomas.pathmann@gmail.com>
2025-09-29 09:56:52 +0200

    disable tree-sitter-context in python buffers

--- .config/nvim/lua/plus/lazy/tree-sitter-context.lua
+++ .config/nvim/lua/plus/lazy/tree-sitter-context.lua
@@ -2,6 +2,8 @@ return {
   "nvim-treesitter/nvim-treesitter-context",
 
   opts = {
-
+    on_attach = function(bufnr)
+      return vim.api.nvim_get_option_value("filetype", { buf = bufnr }) ~= "python"
+    end,
   }
 }
