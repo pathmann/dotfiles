60f6abf74a72ffa3b1927978fd05ce1866854425
pathmann <thomas.pathmann@gmail.com>
2025-11-18 08:39:51 +0100

    use nvim-unception

---
+++ .config/nvim/lua/plus/lazy/unception.lua
@@ -0,0 +1,8 @@
+return {
+    "samjwill/nvim-unception",
+
+    init = function()
+        vim.g.unception_open_buffer_in_new_tab = true
+        vim.g.unception_enable_flavor_text = false
+    end,
+}
