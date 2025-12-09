b66ec165fb62fe38c5e3b0320f9714aff3b322c3
pathmann <thomas.pathmann@gmail.com>
2025-12-09 08:18:23 +0100

    add keymap for latex preview

--- .config/nvim/lua/plus/lazy/texpresso.lua
+++ .config/nvim/lua/plus/lazy/texpresso.lua
@@ -2,4 +2,13 @@ return {
   "let-def/texpresso.vim",
 
   enabled = not require("plus.utils").is_server(),
+
+  config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "tex",
+      callback = function()
+        vim.keymap.set("n", "<leader>lp", "<cmd>TeXpresso %<CR>", { buffer = true, desc = "LaTeX preview" })
+      end,
+    })
+  end,
 }
